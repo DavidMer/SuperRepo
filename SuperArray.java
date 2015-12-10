642a5d71bd6cc6630219a5564947febe5fa1c4bd
David Merlinsky <dmerlinsky@stuy.edu>
2015-12-09 19:43:03 -0500

    superarray lin search and is sorted not wokring

--- SuperArray.java
+++ SuperArray.java
@@ -1,29 +1,8 @@
-// Team SMileyFace -- Maddie Ostergaard, Shaumik Ashraf
-// APCS1 pd9
-// HW40 -- Array of Grade 316
-// 2015-12-2
+public class SuperArray {
 
-
-/*****************************
- * SKELETON for
- * class SuperArray --  A wrapper class for an array. 
- * Maintains functionality:
- *  access value at index
- *  overwrite value at index
- *  report number of meaningful items
- * Adds functionality to std Java array:
- *  resizability
- *  ability to print meaningfully
- *  add item (at end)
- *  insert item
- *  remove item (while maintaining "left-justification")
- *****************************/
-
-public class SuperArray{
- 
     //~~~~~INSTANCE VARS~~~~~
     //underlying container, or "core" of this data structure:
-    private int[] _data;
+    private Comparable[] _data;
 
     //position of last meaningful value
     private int _lastPos;
@@ -31,199 +10,129 @@ public class SuperArray{
     //size of this instance of SuperArray
     private int _size;
 
-		
+    
     //~~~~~METHODS~~~~~
-    //default constructor â€“ initializes 10-item array
-    public SuperArray() 
-    { 
-	_data = new int[10];
-	_lastPos = -1; //flag to indicate no lastpos yet
-	_size = 0;	
+    //default constructor – initializes 10-item array
+    public SuperArray() {
+	_data= new Comparable[10];
+	_lastPos= -1;
+	_size = 0;
     }
-
-		
+    
     //output array in [a,b,c] format, eg
     // {1,2,3}.toString() -> "[1,2,3]"
-    public String toString() 
-    { 
-	String foo = "[";
-	for( int i = 0; i < _size; i++ ) {
-	    foo += _data[i] + ",";
+    public String toString() { 
+	
+	String retstr= "[";
+	for(int i=0; i <= _lastPos;i++){
+	    retstr += _data[i] +",";
 	}
-	//shave off trailing comma
-	if ( foo.length() > 1 )
-	    foo = foo.substring( 0, foo.length()-1 );
-	foo += "]";
-	return foo;
+	if (retstr.length() > 1){
+	    retstr = retstr.substring(0,retstr.length()-1);
+	}
+	retstr+= "]";
+	return retstr;
     }
-
-		
+    
+    
     //double capacity of this SuperArray
-    private void expand() 
-    { 
-	int[] temp = new int[ _data.length * 2 ];
-	for( int i = 0; i < _data.length; i++ )
+    private void expand() {
+	Comparable[] temp = new Comparable[_data.length*2];
+	for(int i = 0; i < _data.length; i++){
 	    temp[i] = _data[i];
+	}
 	_data = temp;
     }
 
-		
+    
     //accessor -- return value at specified index
-    public int get( int index ) { return _data[index]; }
+    public Comparable get( int index ) { return _data[index];}
+    //Add, appends a specified value to the end of the SuperArray
+    public void add(Comparable a){
+	if( _size + 1 < _data.length){//if theres enough room, dont expand just add, 
+	    _data[_lastPos +1]= a;//just
+	    _lastPos++;_size++;
+	}
+	else {//otherwise you need to expand then add.
+	    this.expand();
+	    add(a);
+	}
+	    
+    }
+    //Add at index- Adds a specified value at specifed index, if it exists.
+    public void add(Comparable a, int i){
+	this.expand();//expands just in case, cant hurt right?
 
-		
+	if (i <= _lastPos && i >= 0 ){//if the given index is applicable then 
+	    Comparable temp1;
+	    Comparable temp2 = _data[i];
+	    for(int j = i+1; j <= _lastPos + 1; j++){//move everyhting 1 over
+		temp1 = _data[j];
+		_data[j] = temp2;
+		temp2 = temp1;
+	    }
+	    _data[i] = a;//set the given to the newly created middle slot
+	    _size ++;//increment size and lastpos
+	    _lastPos ++; 
+	}
+	else {
+	    System.out.println("Index not within bounds, cannot add there");
+	}
+    }
     //mutator -- set value at index to newVal, 
     //           return old value at index
-    public int set( int index, int newVal ) 
-    { 
- 	int temp = _data[index];
-	_data[index] = newVal;
-	return temp;
-    }
+    public Comparable set( int index,Comparable newVal ) {Comparable temp = _data[index]; _data[index] = newVal; return temp;}
 
-
-    // ~~~~~~~~~~~~~~ PHASE II ~~~~~~~~~~~~~~
-    //adds an item after the last item
-    public void add( int o) {
-	int newVal = o;
-	int[] new_data = new int[_data.length + 1];
-	for (int i = 0; i <= _lastPos; i ++){
-	    new_data[i] = _data[i];
+    public void remove (int i){
+	if (!(_lastPos < i)){ //aslong as i is within the range
+	    int  x = i; //create a value identical to the original input
+	    while (x < _lastPos){ //while x is within the range of the string
+		_data[x] = _data[x+1];//put whats in the next box into the current one
+		x++;
+	    } 
+	    _size = _size - 1; //fix size
+	    _lastPos = _lastPos - 1; //fix position
 	}
-	new_data[_lastPos +1] = newVal;
-	if (newVal > 0){
-	    _lastPos++;
-	    _size++;
-	}
-	_data = new_data;
-    }
-
-
-    //inserts an item at index
-    //shifts existing elements to the right
-    public void add( int index, int newVal ) {
-	if (index < _size){
 	
-	if (newVal > 0){
-	    _lastPos++;
-	    _size++;
-	}
-	int[] foo = new int[_data.length + 1];
-	for (int i = 0; i < index; i++){
-	    foo[i] = _data[i];
-	}
-	foo[index] = newVal;
-	for (int i = index; i < _data.length; i++){
-	    foo[i + 1] = _data[i];
-	}
-	_data = foo;
+	else{
+	    System.out.println("There's nothing to remove."); //Give a message if something is wrong
 	}
     }
-
-
-    //removes the item at index
-    //shifts elements left to fill in newly-empted slot
-    public void remove( int index ) {
-	if (_data[index] > 0){
-	    _lastPos--;
-	    _size--;
-	}
-	int[] foo = new int[_data.length - 1];
-	for (int i = 0; i < index; i++){
-	    foo[i] = _data[i];
-	}
-	for (int i = index + 1; i < _data.length; i++){
-	    foo[i - 1] = _data[i];
-	}
-	_data = foo;
-	
-    }
-
-
-    //return number of meaningful items in _data
-    public int size() {
-	_size = 0;
-	for (int i = 0; i < _data.length; i ++){
-	    if (_data[i] > 0){
-		_size++;
+    public int linSearch(Comparable c){
+	for(int i = 0; i < _size; i++){
+	    //System.out.println(c);
+	    //System.out.println(_data[i]);
+	    if(c.compareTo(_data[i]) == 0){
+		return i;
 	    }
 	}
-	return _size;
-		
+	return -1;
+    }
+    public boolean isSorted(){
+	for(int i = 0; i < _size-1; i ++){
+	    if (_data[i].compareTo(_data[i+1]) > 0){
+		return false;
+	    }
+	}
+	return true;
     }
 
-
     //main method for testing
-    public static void main( String[] args ) 
-    {
-	SuperArray curtis = new SuperArray();
-	System.out.println("Printing empty SuperArray curtis...");
-	System.out.println(curtis);
-
-	for( int i = 0; i < curtis._data.length; i++ ) {
-	    curtis.set(i,i*2);
-	    curtis._size++; //necessary bc no add() method yet
-	}
-
-	System.out.println("Printing populated SuperArray curtis...");
-	System.out.println(curtis);
-
-	System.out.println("testing get()...");
-	for( int i = 0; i < curtis._size; i++ ) {
-	    System.out.print( "item at index" + i + ":\t" );
-	    System.out.println( curtis.get(i) );
-	}
-
-	System.out.println("Expanded SuperArray curtis:");
-	curtis.expand();
-	System.out.println(curtis);
-	System.out.println("Size = " + curtis.size());
-
-	
-	SuperArray mayfield = new SuperArray();
-	System.out.println("Printing empty SuperArray mayfield...");
-	System.out.println(mayfield);
-
-	  mayfield.add(5);
-	  mayfield.add(4);
-	  mayfield.add(3);
-	  mayfield.add(2);
-	  mayfield.add(1);
-	 
-	  System.out.println("Printing populated SuperArray mayfield...");
-	  System.out.println(mayfield);
-
-	  mayfield.remove(3);
-	  System.out.println("Printing SuperArray mayfield post-remove...");
-	  System.out.println(mayfield);
-	  System.out.println("Size = " + mayfield.size());
-	  mayfield.remove(3);
-	  System.out.println("Printing SuperArray mayfield post-remove...");
-	  System.out.println(mayfield);
-	  System.out.println("Size = " + mayfield.size());
-
-	  mayfield.add(3,99);
-	  System.out.println("Printing SuperArray mayfield post-insert...");
-	  System.out.println(mayfield);
-	  mayfield.add(2,88);
-	  System.out.println("Printing SuperArray mayfield post-insert...");
-	  System.out.println(mayfield);
-	  mayfield.add(1,77);
-	  System.out.println("Printing SuperArray mayfield post-insert...");
-	  System.out.println(mayfield);
-	
-	//*****INSERT ANY ADDITIONAL TEST CALLS HERE*****
-//	  System.out.println("Size = " + mayfield.size());
-
-	ListInt hunter = new SuperArray();
-	  hunter.add(5);
-	  hunter.add(4);
-	  hunter.add(3);
-	  hunter.add(2);
-	  hunter.add(1);
-	 
-	  System.out.println(hunter.get(0));
-	
-	}	
-	
- }//end main
+    public static void main( String[] args ) {
+	//*****INSERT ADEQUATE TEST CALLS HERE*****
+	SuperArray s = new SuperArray();
+	s.add(new Rational(17,1));
+	s.add(new Binary(15));
+	s.add(new Hexadecimal(12));
+	SuperArray r = new SuperArray();
+	r.add(new Rational(1,1));
+	r.add(new Binary(2));
+	r.add(new Binary(3));
+	System.out.println(s);
+	System.out.println(r);
+	System.out.println(s.linSearch(new Hexadecimal( 12)));
+	System.out.println(s.isSorted());
+	System.out.println(r.isSorted());
+    }//end main
+    
+}//end class
